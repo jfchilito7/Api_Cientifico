22938e4abbb186a0b2f7e7b9f6911de98ca87044
jfchilito7 <jfchilito7@gmail.com>
2024-07-16 00:39:30 -0500

    feat: Add router and routes for page navigation

---
+++ src/pages/Character.js
@@ -0,0 +1,39 @@
+import getHash from "../utils/getHash";
+import getData from "../utils/getData";
+
+export const Character = async () => {
+    const id = getHash();
+    const character = await getData(id);
+
+    const {
+        image,
+        name,
+        episode,
+        status,
+        species,
+        gender,
+        origin,
+        location
+    } = character;
+    
+
+    const view = `
+    <div class="Character-inner">
+        <article class="Character-card">
+            <img src="${image}" alt="${name}">
+            <h2>${name}</h2>
+        </article>
+        <article class="Character-card">
+            <h3>Episodes: ${episode.length}</h3>
+            <h3>Status: ${status}</h3>
+            <h3>Species: ${species}</h3>
+            <h3>Gender: ${gender}</h3>
+            <h3>Origin: ${origin.name}</h3>
+            <h3>Last Location: ${location.name}</h3>
+        </article>
+    </div>
+    `
+    return view;
+}
+
+export default Character; //exportamos la funcion Character
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path'); //nos permite trabajar con rutas de archivos y directorios en local o en la nube
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //nos permite trabajar con archivos html
+const CopyWebpackPlugin = require('copy-webpack-plugin'); //nos permite copiar archivos de una carpeta a otra
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //nos permite trabajar con archivos css
 
 module.exports = { // aqui se encuentra toda la configuracion de webpack
     entry: './src/index.js', //aqui se encuentra el codigo inicial y a partir de este se va a generar el archivo final
     output: { //aqui se encuentra el archivo final que se va a generar
-        path: resolve(__dirname, 'dist'), //aqui se encuentra la carpeta donde se va a guardar el archivo final
+        path: path.resolve(__dirname, 'dist'), //aqui se encuentra la carpeta donde se va a guardar el archivo final
         filename: 'main.js', //aqui se encuentra el nombre del archivo final
     },
     resolve: {
@@ -18,16 +20,33 @@ module.exports = { // aqui se encuentra toda la configuracion de webpack
                 use: {
                     loader: 'babel-loader', //aqui se encuentra el loader que vamos a utilizar
                 }
+            },
+            {
+                test: /\.css$i/, //aqui se encuentra la expresion regular que nos permite identificar los archivos con los que vamos a trabajar
+                use: [MiniCssExtractPlugin.loader,//aqui se encuentra los loader que vamos a utilizar
+                    'css-loader',
+                ]
             }
         ]
     },
     plugins: [ //aqui se encuentra los plugins que vamos a utilizar
-        new HtmlWebpackPlugin([ //permite trabajar con los archivos html
+        new HtmlWebpackPlugin( //permite trabajar con los archivos html
             {
             inject: true, //permite inyectar cualquier tipo de archivos
             template: './public/index.html', //aqui se encuentra la ruta del archivo
             filename: './index.html', //aqui se encuentra el nombre del archivo
             }
-        ])
+        ),
+        // new CopyWebpackPlugin( //permite copiar archivos de una carpeta a otra
+        //     {
+        //         patterns: [
+        //             {
+        //                 from: './src/styles/styles.css', //aqui se encuentra la ruta del archivo que vamos a copiar
+        //                 to: '' //aqui se encuentra la carpeta donde vamos a copiar el archivo
+        //             }
+        //         ]
+        //     }
+        // ),
+        new MiniCssExtractPlugin(),
     ]
 }
